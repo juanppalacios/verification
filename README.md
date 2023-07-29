07ce98b5b0b6ce136cc12493b424fc10d3ce3d51
juanppalacios <palaciosvilchis.jpa@gmail.com>
2023-07-28 23:06:55 -0400

    log messages are now known as log events

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 This project will contain a few VHDL packages for verification purposes.
 
 ![log structure](./docs/log_structure.png)
+
+The log has the ability to redirect messages based on its level. For example, an error message will be written to a text file, `stderr.log`, for error-checking purposes. The user can modify the end destination, however.
+
+![log attributes](./docs/log_attributes.png)
--- src/log_pkg.vhd
+++ src/log_pkg.vhd
@@ -7,6 +7,22 @@ package log_pkg is
     type log_level is (none, debug, info, warning, error);
     
     -- log object
+    type log_type;
+    
+    --> root logger
+        --* console handler (default)
+        --! stderr handler (manually added)
+        --~ stdout handler (manually added)
+        
+    -- note: root logger points to child logger
+        
+    --> child logger
+        --* console handler (default)
+        --! stderr handler (manually added)
+        
+    type log_event_type is record
+        
+    end record;
     
     
 end package;
